0d96dc3c8ba0af86a7f16d23d11efa85185fdd93
ishara710 <isharamahapatra2@gmail.com>
2020-05-20 22:17:50 +0530

    Init Commit

---
+++ mongodb.js
@@ -0,0 +1,30 @@
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+const { MongoClient, ObjectID} = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+
+MongoClient.connect(connectionURL,{useUnifiedTopology: true}, (error, client) => {
+    if(error) {
+       return console.log(('Unable to connect to database!'))
+    }
+
+    const db = client.db(databaseName)
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+        $set: {
+            completed: true 
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
+    
+})
+
+
